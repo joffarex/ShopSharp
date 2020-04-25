6908a3b83c3b6fa22d194a0b570c1dc3baad2d17
Tornike Goshadze <joffarex@gmail.com>
2020-04-25 18:48:47 +0000

    Add Database project, context and Product model

---
+++ ShopSharp.Database/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShopSharp.Domain.Models;
+
+namespace ShopSharp.Database
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- ShopSharp.Domain/Models/Product.cs
+++ ShopSharp.Domain/Models/Product.cs
@@ -2,5 +2,9 @@
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Value { get; set; }
     }
 }
