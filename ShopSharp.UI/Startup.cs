9ca3621be55f41f362b70c82cb7c0b7b52213866
Tornike Goshadze <joffarex@gmail.com>
2020-04-25 19:40:20 +0000

    Add migrations

--- ShopSharp.UI/Startup.cs
+++ ShopSharp.UI/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ShopSharp.Database;
 
 namespace ShopSharp.UI
 {
@@ -18,6 +20,9 @@ namespace ShopSharp.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration["DefaultConnection"]));
+
             services.AddRazorPages();
         }
 
