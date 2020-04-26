4c9030f5a44f4bb81addd255353cc027351ac164
Tornike Goshadze <joffarex@gmail.com>
2020-04-26 08:47:28 +0000

    Create Admin controller and add Products services

---
+++ ShopSharp.UI/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopSharp.Application.ProductsAdmin;
+using ShopSharp.Application.ProductsAdmin.ViewModels;
+using ShopSharp.Database;
+
+namespace ShopSharp.UI.Controllers
+{
+    [Route("[controller]")]
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("products")]
+        public IActionResult GetProducts()
+        {
+            return Ok(new GetProducts(_context).Exec());
+        }
+
+        [HttpGet("products/{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            return Ok(new GetProduct(_context).Exec(id));
+        }
+
+        [HttpPost("products")]
+        public IActionResult CreateProduct(ProductViewModel viewModel)
+        {
+            return Ok(new CreateProduct(_context).Exec(viewModel));
+        }
+
+        [HttpPut("products")]
+        public IActionResult UpdateProduct(ProductViewModel viewModel)
+        {
+            return Ok(new UpdateProduct(_context).Exec(viewModel));
+        }
+
+        [HttpDelete("products/{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            return Ok(new DeleteProduct(_context).Exec(id));
+        }
+    }
+}
