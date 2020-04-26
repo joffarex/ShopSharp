fe75b64a9062f6ec00e05625fc85506403859eb9
Tornike Goshadze <joffarex@gmail.com>
2020-04-26 07:58:16 +0000

    Add Create Product functionality

--- ShopSharp.Application/Products/CreateProduct.cs
+++ ShopSharp.Application/Products/CreateProduct.cs
@@ -1,6 +1,29 @@
-﻿namespace ShopSharp.Application.Products
+﻿using System.Threading.Tasks;
+using ShopSharp.Application.Products.ViewModels;
+using ShopSharp.Database;
+using ShopSharp.Domain.Models;
+
+namespace ShopSharp.Application.Products
 {
     public class CreateProduct
     {
+        private readonly ApplicationDbContext _context;
+
+        public CreateProduct(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Exec(ProductViewModel viewModel)
+        {
+            _context.Products.Add(new Product
+            {
+                Name = viewModel.Name,
+                Description = viewModel.Description,
+                Value = decimal.Parse(viewModel.Value)
+            });
+
+            await _context.SaveChangesAsync();
+        }
     }
 }
--- ShopSharp.Domain/Models/Product.cs
+++ ShopSharp.Domain/Models/Product.cs
@@ -1,10 +1,13 @@
-﻿namespace ShopSharp.Domain.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopSharp.Domain.Models
 {
     public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public decimal Value { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")] public decimal Value { get; set; }
     }
 }
