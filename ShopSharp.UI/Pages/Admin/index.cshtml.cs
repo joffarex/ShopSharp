70099adcae6e03d8f9ca1b47b38e07fe63a03beb
Tornike Goshadze <joffarex@gmail.com>
2020-04-26 10:13:37 +0000

    Add Admin products list page

---
+++ ShopSharp.UI/Pages/Admin/index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShopSharp.UI.Pages.Admin
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
