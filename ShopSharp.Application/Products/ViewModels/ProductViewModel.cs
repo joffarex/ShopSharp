83893b61513082386e2ca238578cf303984d6cb6
Tornike Goshadze <joffarex@gmail.com>
2020-04-26 07:57:37 +0000

    Add Product view model

---
+++ ShopSharp.Application/Products/ViewModels/ProductViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopSharp.Application.Products.ViewModels
+{
+    public class ProductViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Value { get; set; }
+    }
+}
