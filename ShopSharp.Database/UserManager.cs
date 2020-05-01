a4d235629dd7cf9b0109fb0e343ad6a397270902
Tornike Goshadze <joffarex@gmail.com>
2020-05-01 15:18:39 +0000

    Extract User Manager from Application layer

---
+++ ShopSharp.Database/UserManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using ShopSharp.Domain.Infrastructure;
+
+namespace ShopSharp.Database
+{
+    public class UserManager : IUserManager
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserManager(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<bool> CreateManagerUser(string username, string password)
+        {
+            var managerUser = new IdentityUser
+            {
+                UserName = username
+            };
+
+            await _userManager.CreateAsync(managerUser, password);
+
+            var managerClaim = new Claim("Role", "Manager");
+            await _userManager.AddClaimAsync(managerUser, managerClaim);
+
+            return true;
+        }
+    }
+}
---
+++ ShopSharp.Domain/Infrastructure/IUserManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace ShopSharp.Domain.Infrastructure
+{
+    public interface IUserManager
+    {
+        Task<bool> CreateManagerUser(string username, string password);
+    }
+}
